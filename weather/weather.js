b1e6d19347f8b617ad3a689fe1c3fe8a8ea65e61
devonborthwick <dborthw1@uwyo.edu>
2024-09-26 14:35:15 -0600

    updates

--- weather/weather.js
+++ weather/weather.js
@@ -1,7 +1,6 @@
 var map = L.map('weathermap').setview([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap = L.tilelayer(basemapUrl, {attribution: '&copy; <a href= "https://' + 'www.openstreetmap.org/copyright">OpenStreetMap<'}); 
-
+var basemapUrl = 'https://.tile.opentopomap.org/{z}/{x}/{y}.png';
+var basemap = L.tilelayer(basemapUrl, {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}); 
 
 var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
