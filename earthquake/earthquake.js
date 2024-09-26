da516db52b9d54f725b1aad16e0c368e727b9682
devonborthwick <dborthw1@uwyo.edu>
2024-09-26 17:01:57 -0600

    fix html quake

--- earthquake/earthquake.js
+++ earthquake/earthquake.js
@@ -5,30 +5,20 @@ L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-   
-// Additional Layer
-// var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?';
-// var radarDisplayOptions = {
-//     layers: 'nexrad-n0r-900913',
-//     format: 'image/png',
-//     transparent: true
-// };
-// var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
+// var quakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php';
 
-var quakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+// $.getJSON(quakeUrl, function(data) {
+//     L.geoJSON(data, {
+//         style: function(feature, latlng){
+//             // var alertColor = 'orange';
+//             // if (feature.properties.severity === 'Severe') alertColor = 'red'; 
+//             // if (feature.properties.severity === 'Extreme') alertColor = 'black';
+//             return l.circleMarker(latlng, geojsonMarkerOptions);
+//         },
+//         onEachFeature: function(feature, layer) {
+//             layer.bindPopup(feature.properties.headline);
 
-$.getJSON(quakeUrl, function(data) {
-    L.geoJSON(data, {
-        style: function(feature, latlng){
-            // var alertColor = 'orange';
-            // if (feature.properties.severity === 'Severe') alertColor = 'red'; 
-            // if (feature.properties.severity === 'Extreme') alertColor = 'black';
-            return l.circleMarker{latlng, geojsonMarkerOptions};
-        },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.headline);
-
-        }
-    }).addTo(map);
-});
+//         }
+//     }).addTo(map);
+// });
