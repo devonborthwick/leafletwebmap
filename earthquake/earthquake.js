898ab0964f51a356e5d7d40c83403eabfe741d13
devonborthwick <dborthw1@uwyo.edu>
2024-09-26 16:50:36 -0600

    readme fix

--- earthquake/earthquake.js
+++ earthquake/earthquake.js
@@ -1,24 +1,33 @@
-var map = L.map('earthquakemap').setview([38, -95], 4);
-var basemapUrl = 'https://.tile.opentopomap.org/{z}/{x}/{y}.png';
-var basemap = L.tilelayer(basemapUrl, {attribution: '&copy; <a href="https://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}); 
+var map = L.map('quakemap').setView([38, -95], 4);
+L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-var quakeUrl=  'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
-var quakeDisplayOptions = {
-    layers: 'nexrad-n0r-900913',
-    format: 'image/png',
-    transparent: true
-};
-var quake = L.tilelayer.wms(quakeUrl, quakeDisplayOptions).addTo(map);
+
+   
+// Additional Layer
+// var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?';
+// var radarDisplayOptions = {
+//     layers: 'nexrad-n0r-900913',
+//     format: 'image/png',
+//     transparent: true
+// };
+// var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+
+
+var quakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
 $.getJSON(quakeUrl, function(data) {
     L.geoJSON(data, {
-        style: function(feature){
-            var alertColor = 'green';
-            if (feature.properties.mag === '>1') alertColor = 'green';  
-            return { color: alertColor };
+        style: function(feature, latlng){
+            // var alertColor = 'orange';
+            // if (feature.properties.severity === 'Severe') alertColor = 'red'; 
+            // if (feature.properties.severity === 'Extreme') alertColor = 'black';
+            return l.circleMarker{latlng, geojsonMarkerOptions};
         },
         onEachFeature: function(feature, layer) {
-        layer.bindPopup(feature.properties.headline);
+            layer.bindPopup(feature.properties.headline);
 
         }
     }).addTo(map);
