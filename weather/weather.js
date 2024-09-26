04eb3811187d665c0624d69f1d5fb99fce4a2188
devonborthwick <dborthw1@uwyo.edu>
2024-09-26 15:45:47 -0600

    updates

--- weather/weather.js
+++ weather/weather.js
@@ -1,14 +1,20 @@
-var map = L.map('weathermap').setview([38, -95], 4);
-var basemapUrl = 'https://.tile.opentopomap.org/{z}/{x}/{y}.png';
-var basemap = L.tilelayer(basemapUrl, {attribution: '&copy; <a href="https://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}); 
+var map = L.map('weathermap').setView([38, -95], 4);
+var basemapUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'; 
+    
+var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
-var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+
+
+   
+
+var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?';
 var radarDisplayOptions = {
     layers: 'nexrad-n0r-900913',
     format: 'image/png',
     transparent: true
 };
-var radar = L.tilelayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
 var weatherAlertsUrl = 'https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active';
 $.getJSON(weatherAlertsUrl, function(data) {
