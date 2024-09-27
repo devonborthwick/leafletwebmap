842318ccdc08b41b83c68f28bffd6e4e91681dc5
devonborthwick <dborthw1@uwyo.edu>
2024-09-26 18:48:52 -0600

    big update

--- weather/weather.js
+++ weather/weather.js
@@ -7,28 +7,28 @@ L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png', {
 
    
 
-// var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?';
-// var radarDisplayOptions = {
-//     layers: 'nexrad-n0r-900913',
-//     format: 'image/png',
-//     transparent: true
-// };
-// var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+var radarUrl=  'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?';
+var radarDisplayOptions = {
+    layers: 'nexrad-n0r-900913',
+    format: 'image/png',
+    transparent: true
+};
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
 
-// var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 
-// $.getJSON(weatherAlertsUrl, function(data) {
-//     L.geoJSON(data, {
-//         style: function(feature){
-//             var alertColor = 'orange';
-//             if (feature.properties.severity === 'Severe') alertColor = 'red'; 
-//             if (feature.properties.severity === 'Extreme') alertColor = 'black';
-//             return { color: alertColor };
-//         },
-//         onEachFeature: function(feature, layer) {
-//             layer.bindPopup(feature.properties.headline);
+$.getJSON(weatherAlertsUrl, function(data) {
+    L.geoJSON(data, {
+        style: function(feature){
+            var alertColor = 'orange';
+            if (feature.properties.severity === 'Severe') alertColor = 'red'; 
+            if (feature.properties.severity === 'Extreme') alertColor = 'black';
+            return { color: alertColor };
+        },
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(feature.properties.headline);
 
-//         }
-//     }).addTo(map);
-// });
+        }
+    }).addTo(map);
+});
