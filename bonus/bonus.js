7d285dad3f110096498840db3ba558d05b349c45
devonborthwick <dborthw1@uwyo.edu>
2024-09-27 11:28:59 -0600

    big change

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,96 +1,190 @@
+// var map = L.map('bonusmap').setView([38, -95], 4);
 
+// // Base layer (replace with your preferred provider)
+// var baseMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//   maxZoom: 17,
+//   minZoom: 0,
+//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
+// var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+
+// var alerts; // Declare variable to hold the actual layer
+
+// var quakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+
+// var quake; // Declare variable to hold the actual layer
+
+// $.getJSON(weatherAlertsUrl, function(data) {
+//   alerts = L.geoJSON(data, {
+//     style: function(feature) {
+//       var alertColor = 'orange';
+//       if (feature.properties.severity === 'Severe') alertColor = 'red'; 
+//       if (feature.properties.severity === 'Extreme') alertColor   
+//  = 'black';
+//       return { color: alertColor };
+//     },
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup(feature.properties.headline);
+//     }
+//   });
+// }).done(function() {
+//   // Add alerts layer to map only after data is fetched
+//   alerts.addTo(map);
+//   alerts.setZIndex(100);
+// });
+
+// $.getJSON(quakeUrl, function(json) {
+//     quake = L.geoJson( json,{
+//         style: function(feature) {
+//                     var mag = feature.properties.mag;
+//                     if (mag >=7.0) {
+//                         return {
+//                             color: "#FF0000"
+//                         }
+//                     }
+//                     if (mag >= 5.0) {
+//                       return {
+//                         color: "#FFA500"
+//                       }; 
+//                     }
+//                     else if (mag >= 3.0) {
+//                       return {
+//                         color: "#FFFF00"
+//                       };
+//                     } else if (mag >= 2.0) {
+//                       return {
+//                         color: "#00FF00"
+//                       };
+//                     } else {
+//                       return {
+//                         color: "#ADD8E6"
+//                       }
+//                     }
+//                   },
+              
+//                   onEachFeature: function(feature, layer) {
+              
+//                     var popupText = "<b>Magnitude:</b> " + feature.properties.mag +
+//                       "<br><b>Location:</b> " + feature.properties.place +
+//                       "<br><b>Time:</b>" + feature.properties.time;
+                    
+//                     layer.bindPopup(popupText);
+//                   },
+              
+//                  pointToLayer: function(feature, latlng) {
+//                     return L.circleMarker(latlng);
+//                   },
+//   });
+// }).done(function() {
+//   // Add quake layer to map only after data is fetched
+//   quake.addTo(map);
+//   quake.setZIndex(100);
+// });
+
+// var baseMaps = {
+//   "Base Map": baseMap
+// };
+
+// var overlayMaps = {
+//   "Earthquakes": quake,
+//   "Weather Alerts": alerts
+// };
+
+// L.control.layers(baseMaps, overlayMaps).addTo(map);
 var map = L.map('bonusmap').setView([38, -95], 4);
-L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+// Base layer (replace with your preferred provider)
+var baseMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  minZoom: 0,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 
-var alerts = $.getJSON(weatherAlertsUrl, function(data) {
-    L.geoJSON(data, {
-        style: function(feature){
-            var alertColor = 'orange';
-            if (feature.properties.severity === 'Severe') alertColor = 'red'; 
-            if (feature.properties.severity === 'Extreme') alertColor = 'black';
-            return { color: alertColor };
-        },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.headline);
-
-        }
-    }).addTo(map)
-});
+var alerts; // Declare variable to hold the actual layer
 
 var quakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
-var quake = $.getJSON(quakeUrl, function(json) {
-     L.geoJson( json,{
-  
-      style: function(feature) {
-        var mag = feature.properties.mag;
-        if (mag >=7.0) {
-            return {
-                color: "#FF0000"
-            }
-        }
-        if (mag >= 5.0) {
-          return {
-            color: "#FFA500"
-          }; 
-        }
-        else if (mag >= 3.0) {
-          return {
-            color: "#FFFF00"
-          };
-        } else if (mag >= 2.0) {
-          return {
-            color: "#00FF00"
-          };
-        } else {
-          return {
-            color: "#ADD8E6"
-          }
-        }
-      },
-  
-      onEachFeature: function(feature, layer) {
-  
-        var popupText = "<b>Magnitude:</b> " + feature.properties.mag +
-          "<br><b>Location:</b> " + feature.properties.place +
-          "<br><b>Time:</b>" + feature.properties.time;
-        
-        layer.bindPopup(popupText);
-      },
-  
-     pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-      },
-    }).addTo(map)
+var quake; // Declare variable to hold the actual layer
+
+$.getJSON(weatherAlertsUrl, function(data) {
+  alerts = L.geoJSON(data, {
+    style: function(feature) {
+      var alertColor = 'orange';
+      if (feature.properties.severity === 'Severe') alertColor = 'red'; 
+      if (feature.properties.severity === 'Extreme') alertColor   
+ = 'black';
+      return { color: alertColor };
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.headline);
+    }
+  });
+}).done(function() {
+  // Add alerts layer to map only after data is fetched
+  alerts.addTo(map);
+  // Set z-index after adding the layer
+  alerts.setZIndex(100);
 });
 
-  var legend = L.control({ position: "bottomleft" });
+$.getJSON(quakeUrl, function(json) {
+  quake = L.geoJson( json,{
+    style: function(feature) {
+        var mag = feature.properties.mag;
+                    if (mag >=7.0) {
+                        return {
+                            color: "#FF0000"
+                        }
+                    }
+                    if (mag >= 5.0) {
+                      return {
+                        color: "#FFA500"
+                      }; 
+                    }
+                    else if (mag >= 3.0) {
+                      return {
+                        color: "#FFFF00"
+                      };
+                    } else if (mag >= 2.0) {
+                      return {
+                        color: "#00FF00"
+                      };
+                    } else {
+                      return {
+                        color: "#ADD8E6"
+                      }
+                    }
+                  },
+              
+                  onEachFeature: function(feature, layer) {
+              
+                    var popupText = "<b>Magnitude:</b> " + feature.properties.mag +
+                      "<br><b>Location:</b> " + feature.properties.place +
+                      "<br><b>Time:</b>" + feature.properties.time;
+                    
+                    layer.bindPopup(popupText);
+                  },
+              
+                 pointToLayer: function(feature, latlng) {
+                    return L.circleMarker(latlng);
+                  },
+  });
+}).done(function() {
+  // Add quake layer to map only after data is fetched
+  quake.addTo(map);
+  // Set z-index after adding the layer
+  quake.setZIndex(100);
+});
+var baseMaps = {
+  "Base Map": baseMap
+};
 
-  legend.onAdd = function(map) {
-    var div = L.DomUtil.create("div", "legend");
-    div.innerHTML += "<h4>Earthquake Magnitude</h4>";
-    div.innerHTML += '<i style="background: #FF0000"></i><span>>=7.0 (Major)</span><br>';
-    div.innerHTML += '<i style="background: #FFA500"></i><span>>=5.0 (Moderate)</span><br>';
-    div.innerHTML += '<i style="background: #FFFF00"></i><span>>=3.0 (Light)</span><br>';
-    div.innerHTML += '<i style="background: #00FF00"></i><span>>=2.0 (Minor)</span><br>';
-    div.innerHTML += '<i style="background: #ADD8E6"></i><span><2.0 (Weak)</span><br>';
- 
-    return div;
-  };
-  
-  legend.addTo(map);
+var overlayMaps = {
+  "Earthquakes": quake,
+  "Weather Alerts": alerts
+};
 
+L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-  
-
-var layerControl = L.control.layers( {
-    "Earthquakes": quake,
-    "Weather Alerts": alerts
-},  {collapsed: false}).addTo(map);
-
-layerControl.addOverlay(quake, alerts);
